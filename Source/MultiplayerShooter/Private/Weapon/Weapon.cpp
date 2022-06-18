0eda8562e490c0c6e41251491cc611ddb142ea1c
Eduard Ciofu <cfedward7@gmail.com>
2022-06-18 14:12:31 +0200

    Weapon class added

---
+++ Source/MultiplayerShooter/Private/Weapon/Weapon.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/Weapon.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	AreaSphere->SetupAttachment(RootComponent);
+	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/MultiplayerShooter/Public/Weapon/Weapon.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+class USphereComponent;
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial		UMETA(DisplayName = "Initial State"),
+	EWS_Equipped	UMETA(DisplayName = "Equipped"),
+	EWS_Dropped		UMETA(DisplayName = "Dropped"),
+
+	EWS_MAX			UMETA(DisplayName = "DefaultMAX")
+};
+
+UCLASS()
+class MULTIPLAYERSHOOTER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USphereComponent* AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+
+public:	
+	
+
+};
